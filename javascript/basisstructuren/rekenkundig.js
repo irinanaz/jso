64c4eaf0b62a43139d4365fdffc879df5757dc84
irinanaz <naz_irina@msn.com>
2017-10-24 09:28:55 +0200

    rekenkundig.js met commentaar

--- javascript/basisstructuren/rekenkundig.js
+++ javascript/basisstructuren/rekenkundig.js
@@ -4,20 +4,21 @@ var x = 1, y = 2, z = 3;
 var som;
 var resultaat1, resultaat2;
 
-som = x + y;
+som = x + y;//3
 console.log(som);
-som = som + x;
+som = som + x;     //4
 console.log(som);
-som += x;
+som += x;        //5
 console.log(som);
-som++;
+som++; //6
 console.log(som);
 console.log(som++);
 console.log(som);
-console.log(++som);
+console.log(++som);// postfix-doe de rest van de instructies en dan +1 -> 6 komt op het scherm en som wordt 7
+/*console.log(som++);// prefix - doet +1 en dan de rest van instructies  -> 8 komt op het scherm*/
 console.log(som);
 
-resultaat1 = x + y * z;
-console.log("zonder haakjes : " + resultaat1);
-resultaat2 = (x + y) * z;
+resultaat1 = x + y * z; 
+console.log("zonder haakjes : " + resultaat1); //7
+resultaat2 = (x + y) * z;//9
 console.log("met haakjes : " + resultaat2);        
