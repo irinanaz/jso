5aed17638e8d4a2ca8b60d9cb84d0b211a652f7a
irinanaz <naz_irina@msn.com>
2017-10-24 10:01:50 +0200

    VB IF geleerd

--- javascript/basisstructuren/if01.js
+++ javascript/basisstructuren/if01.js
@@ -1,8 +1,8 @@
 ﻿'use strict';
 
 var datum = new Date();
-var uur = datum.getHours();
-if (uur < 12) {
+var uur = datum.getHours(); // vraagt een huidige datum en tijd,.nl. de uren hier. Zet de Uur in datum variabele en geeft de waarde aan datum. 
+if (uur < 12) { // na IF altijd met de ronde haken
 	console.log("Goedemorgen");
 } else {
 	console.log("Goedenamiddag");
--- javascript/basisstructuren/if02.js
+++ javascript/basisstructuren/if02.js
@@ -2,7 +2,7 @@
 
 var datum = new Date();
 var uur = datum.getHours();
-if (uur == 9) {
+if (uur == 9) {// == vergelijking. === kijkt naar het zelfde type, en als ja dan vergelijkt verder.=== past de types niet.
 	console.log("Tussen 9 en 10 kan je eens naar koffie zien");
 } else if (uur == 12) {
 	console.log("Tussen 12 en 1 eet iedereen");
@@ -10,4 +10,17 @@ if (uur == 9) {
 	console.log("Tussen 6 en 7 begint een werkmens te leven");
 } else {
 	console.log("Werk maar rustig verder");
+}
+
+if (uur!=12) {
+	console.log("de middagpauze is niet bezig");
+}else{
+	console.log('het is middagpauze');
+}
+if (uur>=9 && uur<=12) { // AND 
+	console.log("de werkvoormiddag is bezig");
+}
+
+if (uur<9 || uur>12) {  // OF
+	console.log("buiten de werkuren");
 }
