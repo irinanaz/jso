aff0404a5bd39091a703ed4580658b5bfb34706b
irinanaz <naz_irina@msn.com>
2017-10-25 14:03:30 +0200

    oef

--- javascript/basisstructuren/Oef/OefFies02.js
+++ javascript/basisstructuren/Oef/OefFies02.js
@@ -1,15 +1,15 @@
 'use strict';
 
 var toetsenbord = require('readline-sync');
-
+//var wisselkoers = 40.3399
 
 function BEFnaarEURO(x){
-        var xinEuro=x/40.3399;
+        var xinEuro=x/40.3399; // wisselkoers
     return xinEuro;
 }
 function EUROnaarBEF(y){
-    var xinBef=y*40.3399;
-    return xinBef;
+    var xinBef=y*40.3399;  // wisselkoers
+    return xinBef;          // mag ook zo: reteurn y*wisselkoers, en vorige lijn moet dan weg;
 }
 
 var bef = toetsenbord.question('Tik prijs voor een liter melk(in BEF) in: ');
