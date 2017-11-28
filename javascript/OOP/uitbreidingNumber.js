0d02b456903b5a7877adc0b0d22fde3b5a308ee8
irinanaz <naz_irina@msn.com>
2017-11-28 15:15:23 +0100

    oef

---
+++ javascript/OOP/uitbreidingNumber.js
@@ -0,0 +1,21 @@
+﻿'use strict';
+
+
+
+Number.pad = function (num, size, char){
+	var sTmp = ""+num;
+	while (sTmp.length < size) {
+        sTmp = char + sTmp;
+       
+    } return sTmp;
+}
+
+var getal =5;
+console.log(Number.pad(getal,2, '0'));
+getal = 10;
+console.log(Number.pad(getal,2, '0'));
+getal = 100;
+console.log(Number.pad(getal,2, '0'));
+console.log(Number.pad(getal,4, '0'));
+// var s = Number.pad(12, 4,'0');
+// console.log(s);
