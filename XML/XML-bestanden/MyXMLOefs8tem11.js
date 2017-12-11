cdfa0d08fba11672ecdb6ca1ff3613e9fa6ed276
irinanaz <naz_irina@msn.com>
2017-12-11 10:20:08 +0100

    XPath commentaren

--- XML/XML-bestanden/MyXMLOefs8tem11.js
+++ XML/XML-bestanden/MyXMLOefs8tem11.js
@@ -13,25 +13,19 @@ window.onload = function(){
 }
 
 function runOef11(){
-    var xmlString = loadXMLText(FILENAME11);
-    // var xmlString = "<smurfen>" +
-    // "<smurf><naam>Grote Smurf</naam><omschrijving>leider van de smurfen</omschrijving><kenmerk>draagt als enige een rode broek en een rode muts</kenmerk></smurf>"+
-    // "<smurf><naam>Smurfin</naam><omschrijving>vrouwelijke smurf</omschrijving><kenmerk>blond haar, wit jurkje</kenmerk></smurf>"+
-    // "</smurfen>";
+    var xmlString = loadXMLText(FILENAME11); // geeft een string terug van de html pagina
+  
     var domParser = new DOMParser();
     /*  Volgens de documentatie van MDN (https://developer.mozilla.org/en-US/docs/Web/API/DOMParser)
     is DOMParser "experimental", maar de huidige browsers ondersteunen DOMParser wel  */
+
+
     var doc = domParser.parseFromString(xmlString, "application/xml");
-    var output = doc.documentElement.children.length + " " + doc.documentElement.nodeName + " geladen:";
+    var output = doc.getElementsByTagName("a") /*;
     for(var i=0; i < doc.documentElement.children.length; i++){
         output += "\n" + doc.documentElement.children[i].firstChild.textContent;
     }
-    alert(output);
-
-
-    
-
-
+    alert(output);*/
 }
 
 
@@ -39,7 +33,7 @@ function runOef10(){
     var doc = loadXMLDoc(FILENAME10);
     var prijzen = doc.getElementsByTagName("prijs");
 
-    for (var i =0; i<prijzen.length; i++){
+    for (var i =0; i<prijzen.length; i++)  {
         prijzen[i].innerHTML = parseFloat(prijzen[i].textContent) * 1.05;
         console.dir( prijzen[i].textContent );
     }
@@ -48,7 +42,9 @@ function runOef10(){
 
 
 function runOef8(){
+    // om van lege doc te beginnen:
     doc = document.implementation.createDocument("","",null); // of return doc op het einde;
+
     var newhtml = doc.createElement("html");
     var newHead = doc.createElement("head");
     var newTitle = doc.createElement("title");
@@ -67,10 +63,7 @@ function runOef8(){
     
     newBody.appendChild(newLabel);
     newBody.appendChild(newLink);
-    // var newP = doc.createElement("p");
-    // var Ptext = doc.createTextNode("De eerste paragraaf");
-    // newP.appendChild(Ptext);
-    // newBody.appendChild(newP);
+   
     newhtml.appendChild(newBody);
     doc.appendChild(newhtml);
     
