29b1563c324b1d5f65e0e72ea1df8afeda386710
irinanaz <naz_irina@msn.com>
2018-01-24 09:14:11 +0100

    opl van Ann

--- Node.js/mongodb02.js
+++ Node.js/mongodb02.js
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
 /**  voor MongoDb 3+ */
-=======
-/*  mongodb driver V3+ */ 
->>>>>>> ead5ce85740258fc5938160d9b024de8ab675876
 /*
  * vooraf:  npm install mongodb
  */
 
-<<<<<<< HEAD
 'use strict';
 var toetsenbord = require('readline-sync');
 var mongoClient = require('mongodb').MongoClient;
@@ -37,35 +32,33 @@ mongoClient.connect(url, function (err, client) { // poort en machine, maar db n
 }
 toonRestaurants(myKeuken);
 
-// injecties geven geen groot probleem want alles wat naar mongo gaat wordt een string
-//toonRestaurants({$gte:''});
-//asyncroon dus instructies die hier komen worden parallel uitgevoerd:
-console.log('-----------------------wat denke hiervan?--------------------');
-=======
-// zie ook http://mongodb.github.io/node-mongodb-native/3.0/quick-start/quick-start/
+// // injecties geven geen groot probleem want alles wat naar mongo gaat wordt een string
+// //toonRestaurants({$gte:''});
+// //asyncroon dus instructies die hier komen worden parallel uitgevoerd:
+// console.log('-----------------------wat denke hiervan?--------------------');
+// // zie ook http://mongodb.github.io/node-mongodb-native/3.0/quick-start/quick-start/
 
-'use strict';
+// 'use strict';
 
-var mongoClient = require('mongodb').MongoClient;
-var url = 'mongodb://localhost:27017';
+// var mongoClient = require('mongodb').MongoClient;
+// var url = 'mongodb://localhost:27017';
 
-function toonRestaurants(cuisine) {
-  mongoClient.connect(url, function (err, client) {
-    console.log("Connected successfully to server");
-    var db = client.db('test');
-    var collection = db.collection('restaurants');
-    collection.find({ cuisine: cuisine }).toArray(function (err, docs) {
-      console.log("Restaurant document(s) found:");
-      docs.forEach(function (element) {
-        console.log('%s (%s), %s', element.name, element.cuisine, element.address ? element.address.street : "");
-      });
-      client.close();
-    });
-  });
-}
+// function toonRestaurants(cuisine) {
+//   mongoClient.connect(url, function (err, client) {
+//     console.log("Connected successfully to server");
+//     var db = client.db('test');
+//     var collection = db.collection('restaurants');
+//     collection.find({ cuisine: cuisine }).toArray(function (err, docs) {
+//       console.log("Restaurant document(s) found:");
+//       docs.forEach(function (element) {
+//         console.log('%s (%s), %s', element.name, element.cuisine, element.address ? element.address.street : "");
+//       });
+//       client.close();
+//     });
+//   });
+// }
 
-toonRestaurants("Chinese");
+// toonRestaurants("Chinese");
 
-console.log("********** wat denk je hiervan ? **********");
-toonRestaurants({$gte:""});   // NoSQL injection
->>>>>>> ead5ce85740258fc5938160d9b024de8ab675876
+// console.log("********** wat denk je hiervan ? **********");
+// toonRestaurants({$gte:""});   // NoSQL injection
--- Node.js/server-express-restaurants01.js
+++ Node.js/server-express-restaurants01.js
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-
-var express = require('express');
-var app = express();
-
-function getRecords(callback) {
-    var result = {};
-    var mongoClient = require('mongodb').MongoClient;
-    var url = 'mongodb://localhost:27017';
-
-        mongoClient.connect(url, function (err, client) { 
-            var db = client.db('test'); 
-            console.log("Connected successfully to server");
-            var collection = db.collection('restaurants');
-            collection.find({}).project({'name':1,'address':1,'borough':1,'cuisine':1,'_id':0}).toArray(function (err, docs) {  
-                console.log("Restaurant document(s) found:");
-                    if (err) {
-                        callback(err, {});
-            
-                    } else {
-                        callback(null, docs);
-                    }
-                
-                client.close();
-                
-            });
-        });
-    
-
-}
-
-=======
 var mongoClient = require('mongodb').MongoClient;
 var app = require('express')();
 
@@ -37,82 +5,12 @@ var app = require('express')();
 var url = 'mongodb://localhost:27017';
 
 // enable cross domain calls (CORS = cross origin resource sharing)
->>>>>>> ead5ce85740258fc5938160d9b024de8ab675876
 app.all('/*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
-<<<<<<< HEAD
-app.get('/restaurants.json', function (req, res) {
-    console.log('request received');
-    getRecords(function (err, docs) {
-        var result;
-        if (err) {
-            console.log('Error while performing query.');
-            result = {};
-        }
-        else {
-            console.log("Sending data to client:");
-            result = docs;
-        }
-        
-
-        res.end(JSON.stringify({ data: result }));
-    });
-});
-
-var server = app.listen(1337, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log("Example app listening at http://%s:%s", host, port)
-});
-/*********************************************************************** */
-// var express = require('express');
-// var app = express();
-
-// 'use strict';
-// var mongoClient = require('mongodb').MongoClient;
-// var url = 'mongodb://localhost:27017';
-
-// function toonRestaurants(keuken) {
-//     // Use connect method to connect to the server
-//     mongoClient.connect(url, function (err, client) { // poort en machine, maar db nog niet.
-//         var db = client.db('test'); //bepalen van db bij client -welke we willen gebruiken
-//         console.log("Connected successfully to server");
-//         // Get the restaurants collection
-//         var collection = db.collection('restaurants');
-//         // Find all documents
-//         collection.find({ cuisine: keuken }).toArray(function (err, docs) {  // node werkt asynchrone, 
-//             // als de array klaar is dan begint de werk met doc
-//             console.log("Restaurant document(s) found:");
-//             docs.forEach(function (element) {
-//                 console.log('%s (%s), %s', element.name, element.cuisine, element.address ? element.address.street : "");
-
-//             });
-//             var jsonDB = JSON.stringify({ data: docs });
-//             client.close();//JSON.stringify({ data: docs })
-//             return jsonDB;
-//         });
-//     });
-// }
-
-
-// app.get('/restaurant', function (req, res) {
-//     console.log('request received');
-//     var jsonDB = toonRestaurants('Italian');
-//     res.end("ok");
-// });
-
-// var server = app.listen(1337, function () {
-//     var host = server.address().address;
-//     var port = server.address().port;
-
-//     console.log("Example app listening at http://%s:%s", host, port)
-// });
-=======
 // GET /restaurants
 app.get('/restaurants', verwerkRequestVoorRestaurants);
 app.listen(8888);
@@ -129,4 +27,3 @@ function verwerkRequestVoorRestaurants(request, response) {
         });
     });
 }
->>>>>>> ead5ce85740258fc5938160d9b024de8ab675876
