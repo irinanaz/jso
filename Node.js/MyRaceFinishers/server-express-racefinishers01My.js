9e297fb82e7a5e092964738de68a010a466be314
irinanaz <naz_irina@msn.com>
2018-01-30 15:18:40 +0100

    racers werkt

--- Node.js/MyRaceFinishers/server-express-racefinishers01My.js
+++ Node.js/MyRaceFinishers/server-express-racefinishers01My.js
@@ -2,7 +2,7 @@ var mongoClient = require('mongodb').MongoClient;
 var app = require('express')();
 var fs = require("fs");
 // Connection URL naar DB
-// var url = 'mongodb://localhost:27017';
+var url = 'mongodb://localhost:27017';
 
 // enable cross domain calls (CORS = cross origin resource sharing)
 app.all('/*', function (req, res, next) {
@@ -11,14 +11,30 @@ app.all('/*', function (req, res, next) {
     next();
 });
 // parser voor post methode:
-//var bodyParser = require('body-parser');
-//app.use(bodyParser.urlencoded({
-//   extended: true
-//}));
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
-//app.get('/raceFinishers01My.html', verwerkRequestVoorRestaurants);
+
+
+
+app.post('/addRunner', function (req, res) {
+    fs.readFile( 'raceFinishers.json', 'utf8', function (err, data) { 
+        console.log( data );
+        console.log('%s %s wordt toegevoegd', req.body.txtFirstName, req.body.txtLastName);
+        res.end( data );
+    });
+    res.end('{"message" : "Added Successfully", "status" : 200}');
+});
+
+
+app.post('/racerNew', insertRacersDB);
+app.get('/racersVrouwen', sorteerRacersVDB);
+app.get('/racersMannen', sorteerRacersMDB);
+app.get('/racers', sorteerRacersDB);
+
 app.get('/raceFinishersJson', function(req, res) {
 	fs.readFile( 'raceFinishers.json', 'utf8', function (err, data) { 
         console.log( data );
@@ -26,11 +42,11 @@ app.get('/raceFinishersJson', function(req, res) {
     });
 });
 
-// app.get('/raceFinishers01My.html', function(req, res) {
-	// res.sendFile(__dirname + "/raceFinishers01My.html");
-// });
+app.get('/racersort01My.html', function(req, res) {
+	res.sendFile(__dirname + "/racersort01My.html");
+});
 
-// app.listen(8888);
+app.listen(8888);
 
 var server = app.listen(1337, function () {
 	var host = server.address().address;
@@ -39,5 +55,85 @@ var server = app.listen(1337, function () {
 	console.log("Example app listening at http://%s:%s", host, port)
 });
 
+function verwerkRacersDB(request, response) {
+    mongoClient.connect(url, function (err, client) {
+        console.log("Connected successfully to server of RacersDB");
+        var db = client.db('test');
+        var collection = db.collection('racers');
+        collection.find({}).project({ _id: 0 }).toArray(function (err, docs) {
+            console.log("Racers document(s) found");
+            response.send(JSON.stringify(docs));
+            client.close();
+        });
+    });
+}
 
- 
+function insertRacersDB(request, response) {  //veld, oldData, newData
+    var fnaam = request.body.txtName;
+    var fvnaam = request.body.txtFirstName;
+    var fgender = request.body.idGender;
+    var furen = request.body.nmbHours;
+    var fmin =request.body.nmbMinutes;
+    
+    mongoClient.connect(url, function (err, client) {
+      console.log("Connected successfully to server of RacersDB: NEW");
+      var db = client.db('test');
+      var collection = db.collection('racers');
+      collection.insertOne({voornaam : fvnaam, naam : fnaam, uren : furen, minuten : fmin, gender: fgender}, function (err, docs) {
+        console.log("Racers document(s) added:");
+      if(err) throw err;
+      // if (docs.acknowledged==true){
+          console.log(docs.n  +" new record inserted successfully");	//It will console the number of rows updated
+          //}//console.log(docs);
+        client.close();
+        response.end('berichtje');
+      });
+    });
+  }
+function refreshForm(request,response){
+    
+}
+
+
+
+
+function sorteerRacersDB(request, response) {
+    mongoClient.connect(url, function (err, client) {
+        console.log("Connected successfully to server of RacersDB: ALL");
+        var db = client.db('test');
+        var collection = db.collection('racers');
+        collection.find({}).project({ _id: 0 }).sort({'uren':1, 'minuten':1}).toArray(function (err, docs) {
+            console.log("Racers document(s) found");
+            response.send(JSON.stringify(docs));
+            client.close();
+            response.end('{"message" : "Sorted Successfully", "status" : 200}');
+        });
+    });
+}
+function sorteerRacersMDB(request, response) {
+    mongoClient.connect(url, function (err, client) {
+        console.log("Connected successfully to server of RacersDB: Mannen");
+        var db = client.db('test');
+        var collection = db.collection('racers');
+        collection.find({'gender':'man'}).project({ _id: 0 }).sort({'uren':1, 'minuten':1}).toArray(function (err, docs) {
+            console.log("Racers document(s) found");
+            response.send(JSON.stringify(docs));
+            client.close();
+            response.end('{"message" : "Sorted Successfully", "status" : 200}');
+        });
+    });
+}
+function sorteerRacersVDB(request, response) {
+    mongoClient.connect(url, function (err, client) {
+        console.log("Connected successfully to server of RacersDB: Vrouwen");
+        var db = client.db('test');
+        var collection = db.collection('racers');
+        collection.find({'gender':'woman'}).project({ _id: 0 }).sort({'uren':1, 'minuten':1}).toArray(function (err, docs) {
+            console.log("Racers document(s) found");
+            response.send(JSON.stringify(docs));
+            client.close();
+            response.end('{"message" : "Sorted Successfully", "status" : 200}');
+        });
+    });
+}
+
